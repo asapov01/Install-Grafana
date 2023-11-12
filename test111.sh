58bcb4e772b732177fabfb22efebc82304775adf
Asapov <137000528+asapov01@users.noreply.github.com>
2023-11-12 17:46:40 +0200

    Update and rename test11.sh to test111.sh

--- test111.sh
+++ test111.sh
@@ -97,9 +97,9 @@ read -p "Введіть IP: " ip
             read -p "Введіть IP: " ip
             read -p "Введіть назву для відображення в Grafana: " label
 
-            echo "  - targets: ['$ip:9100']" >> $HOME/prometheus/prometheus.yml
-            echo "    labels:" >> $HOME/prometheus/prometheus.yml
-            echo "      label: \"$label\"" >> $HOME/prometheus/prometheus.yml;;
+            echo "  - targets: ['$ip:9100']" >> /root/prometheus/prometheus.yml
+            echo "    labels:" >> /root/prometheus/prometheus.yml
+            echo "      label: \"$label\"" >> /root/prometheus/prometheus.yml;;
         [Nn]* ) break;;
         * ) echo "Будь ласка, введіть Y або N.";;
     esac
